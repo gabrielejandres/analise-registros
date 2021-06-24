c90b982ed412da35f8d222546fbe6ae0c5b100a5
Gabriele Jandres Cavalcanti <gabrielejandres@gmail.com>
2021-06-24 17:29:27 -0300

    docs: update docstrings

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import requests
 
 def getCountryByCode(code):
+    '''
+        Returns the country corresponding to the IDD code entered
+
+        :param code: String with IDD code
+        :return: The country
+    '''
+
     response = requests.get(f'https://restcountries.eu/rest/v2/callingcode/{code}')
-    if response.status_code != 200:
-        raise Exception('Erro ao consultar API de códigos DDI')
-    else:
+
+    if response.status_code == 200:
         response = response.json()
         return response[0]['translations']['br']
+    else:
+        raise Exception('Erro ao consultar API de códigos DDI')
+
 
 def updateDataOfCountry(country, status):
+    '''
+        Updates the total number of users and number of active users by country
+
+        :param country: The country with data to be updated
+        :param status: User status indicating if it is active or not
+    '''
+
     if usersByCountry.get(country) is None:
         usersByCountry[country] = activesByCountry[country] = 0
 
@@ -16,19 +32,31 @@ def updateDataOfCountry(country, status):
     if status == "assinado":
         activesByCountry[country] += 1
 
+
 def handleFileReading(nameOfFile):
+    '''
+        Reads the input file and identifies information needed to update each country's data
+
+        :param nameOfFile: Input file name
+    '''
+
     file = open(nameOfFile, 'r')
 
     for line in file:
-        ddi = line[0] + line[1]
+        idd = line[0] + line[1]
         status = line.split()[1]
-        country = getCountryByCode(ddi)
+        country = getCountryByCode(idd)
 
         updateDataOfCountry(country, status)
 
     file.close()
 
+
 def createReportFile():
+    '''
+        Creates a report file with the total number of users and the number of active users in each country
+    '''
+
     newFile = open("out.txt", "w")
 
     for key in usersByCountry:
@@ -38,9 +66,12 @@ def createReportFile():
     print("Análise dos dados enviada para o arquivo out.txt")
     newFile.close()
 
+
+# Dictionaries to associate the total number of users and the number of active users per country
 usersByCountry = {}
 activesByCountry = {}
 
+# Main flow
 name = input("Digite o nome do arquivo de registros de entrada: ")
 handleFileReading(name)
 createReportFile()
